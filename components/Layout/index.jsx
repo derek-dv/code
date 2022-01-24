cd30c94ed5384096ab9310184e13bd167a2f00b4
derek david <derekmiracledavid@hotmail.com>
2022-01-24 12:14:19 +0100

    footer added to page

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 
+
 // Components
 import Navbar from "./Navbar";
-import Notifications from "../UI/Notifications";
+import Footer from "./Footer"
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <Notifications />
+    <div className="h-screen layout">
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </div>
   );
 };
