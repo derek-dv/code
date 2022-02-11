fbb54159976bf300185fc8e2b0e39cf65279dad1
derek david <derekmiracledavid@hotmail.com>
2022-02-11 07:58:34 +0100

    removed unused files

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -6,7 +6,7 @@ const connection = {};
 async function dbConnect() {
   if (connection.isConnected) return;
 
-  const db = await mongoose.connect(process.env.MONGO_URI);
+  const db = await mongoose.connect('mongodb+srv://derek:derek@cluster0.mitgk.mongodb.net/myFirstDatabase?>');
 
   connection.isConnected = db.connections[0].readyState;
   console.log(connection);
