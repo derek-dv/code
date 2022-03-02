1c83a04df96976b87ab2a9182d406230755b6c5e
derek david <derekmiracledavid@hotmail.com>
2022-03-02 21:35:09 +0100

    rate limit added

--- pages/api/files/[fileId].js
+++ pages/api/files/[fileId].js
@@ -1,54 +1,48 @@
 import dbConnect from "../../../utils/dbconnect";
 import File from "../../../model/File";
+import handler from "../../../middleware/auth";
 
 dbConnect();
 
-export default async (req, res) => {
-  const httpMethod = req.method;
-  const id = req.query.fileId;
+handler
+  .get(async (req, res) => {
+    const id = req.query.fileId;
+    try {
+      const file = await File.findOne({ _id: id });
+      res.status(200).json(file);
+      console.log(file);
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ error: `file with id ${id} not found` });
+    }
+  })
+  .put(async (req, res) => {
+    const id = req.query.fileId;
+    try {
+      const temp = await File.findOne({ _id: id });
+      const file = await File.findOneAndUpdate(
+        { _id: id },
+        {
+          fileName: req.body.fileName ? req.body.fileName : temp.fileName,
+          code: req.body.code ? req.body.code : temp.code,
+        }
+      );
+      file.save();
+      res.send(file);
+    } catch (err) {
+      console.log(err);
+    }
+  })
+  .delete(async (req, res) => {
+    const id = req.query.fileId;
 
-  switch (httpMethod) {
-    case "GET":
-      try {
-        const file = await File.findOne({ _id: id });
-        res.status(200).json(file);
-        console.log(file);
-      } catch (error) {
-        console.log(error);
-        res.status(404).json({ error: `file with id ${id} not found` });
-      }
-      break;
+    File.deleteOne({ _id: id })
+      .then((files) => {
+        res.json({ message: "File deleted" });
+      })
+      .catch(() => {
+        res.status(404).json({ error: "File with ID not found" });
+      });
+  });
 
-    case "PUT":
-      try {
-        const temp = await File.findOne({ _id: id });
-        const file = await File.findOneAndUpdate(
-          { _id: id },
-          {
-            fileName: req.body.fileName ? req.body.fileName : temp.fileName,
-            code: req.body.code ? req.body.code : temp.code,
-          }
-        );
-        file.save();
-        res.send(file);
-      } catch (err) {
-        console.log(err);
-      }
-      break;
-
-    case "DELETE":
-      File.deleteOne({ _id: id })
-        .then((files) => {
-          res.json({ message: "File deleted" });
-        })
-        .catch(() => {
-          res.status(404).json({ error: "File with ID not found" });
-        });
-
-      break;
-
-    default:
-      res.status(403).send(`HTTP method ${httpMethod} not allowed`);
-      break;
-  }
-};
+export default handler;
--- pages/api/files/author/[author_id].js
+++ pages/api/files/author/[author_id].js
@@ -1,29 +1,21 @@
 import dbConnect from "../../../../utils/dbconnect";
 import File from "../../../../model/File";
+import handler from "../../../../middleware/auth";
 
 dbConnect();
 
-export default async (req, res) => {
-  const httpMethod = req.method;
+handler.get(async (req, res) => {
   const id = req.query.author_id;
-
-  switch (httpMethod) {
-    case "GET":
-      try {
-        const file = await File.find({ author_id: id });
-        console.log(file);
-        const totalFiles = file.length;
-        const fileSize = file.reduce((a, c) => a + Number(c.fileSize), 0);
-        console.log(fileSize);
-        res.status(200).json({ files: file, totalFiles, fileSize });
-      } catch (error) {
-        console.log(id);
-        res.status(404).json({ error: `Author with id ${id} not found` });
-      }
-      break;
-
-    default:
-      res.status(403).send(`HTTP method ${httpMethod} not allowed`);
-      break;
+  try {
+    const file = await File.find({ author_id: id });
+    console.log(file);
+    const totalFiles = file.length;
+    const fileSize = file.reduce((a, c) => a + Number(c.fileSize), 0);
+    console.log(fileSize);
+    res.status(200).json({ files: file, totalFiles, fileSize });
+  } catch (error) {
+    console.log(id);
+    res.status(404).json({ error: `Author with id ${id} not found` });
   }
-};
+});
+export default handler;
