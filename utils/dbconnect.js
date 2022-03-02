6ea35a411ff477a522ab538107e36730275174f3
derek david <derekmiracledavid@hotmail.com>
2022-03-02 22:17:34 +0100

    m

--- utils/dbconnect.js
+++ utils/dbconnect.js
@@ -6,8 +6,8 @@ const connection = {};
 async function dbConnect() {
   if (connection.isConnected) return;
 
-  const db = await mongoose.connect('mongodb+srv://derek:derek@cluster0.mitgk.mongodb.net/myFirstDatabas?retryWrites=true&w=majority' || process.env.MONGO_URI);
-
+  const db = await mongoose.connect('mongodb+srv://derek:derek@cluster0.mitgk.mongodb.net/aol?retryWrites=true&w=majority' || process.env.MONGO_URI);
+//new
   connection.isConnected = db.connections[0].readyState;
   console.log(connection);
 }
